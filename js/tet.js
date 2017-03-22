a5bd00fc710a51ece8b7be1143abfa562e6e2901
fengye <1270763370@qq.com>
2017-03-22 10:00:20 +0800

    gulp

---
+++ js/tet.js
@@ -0,0 +1,25 @@
+/*
+* @Author: anchen
+* @Date:   2017-03-21 18:03:56
+* @Last Modified by:   anchen
+* @Last Modified time: 2017-03-22 09:42:49
+*/
+
+window.onload=function(){
+    var  json =  {
+        "project" : "Grunt",
+        "localserver" : {
+            "host" : "localhost",
+            "port" : "8081"
+        },
+        "tinypngapi": "Tinypng API KEY",
+        "sftp" : {
+            "host" : "8.8.8.8",
+            "user" : "username",
+            "port" : "22",
+            "key" : "~/.ssh/sdfsfdsf",
+            "remotePath" :"/"
+        }
+     }   ;
+     console.log("2");
+}
